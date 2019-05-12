430c21f8e4f532e5c5a8ce6a9637a7129d7cc339
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-12 03:16:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+
+  @@account = []
+  
+  attr_accessor :sender, :receiver, :amount, :status
+ 
+
+def initialize(sender, receiver, amount = 50)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+  
+  @@account << self
+end
+
+def valid?
+  if @amount >= 50 || @status == "open"
+     if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+end
+
+def execute_transaction
+  sender.balance -= amount || receiver.balance += amount
+  if ! sender.valid?
+    @status == "rejected"
+    "Transaction rejected. Please check your account balance."
+end
+end
+
+def reverse_transfer
+ 
+end
 end
