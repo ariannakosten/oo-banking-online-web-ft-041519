51a2c2b2eb3ec34276a54f2583324e7aa3442e1c
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-12 03:49:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,9 +28,9 @@ def execute_transaction
 #sender.balance -= amount || receiver.balance += amount
  if valid? && status == "pending"
       if amount < self.sender.balance
-        @sender.balance -= amount
-        @receiver.balance += amount
-        @status = "complete"
+        self.sender.balance -= amount
+        self.eceiver.balance += amount
+        self.status = "complete"
       else
    # !(sender.valid?)
     self.status == "rejected"
